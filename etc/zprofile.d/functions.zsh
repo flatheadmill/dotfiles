f72fc82b1eb590819cc299fe81fcf939832d3def
Alan Gutierrez <alan@prettyrobots.com>
2013-01-09 21:09:01 -0500

    Update my history cleaning function for zsh.

--- etc/zprofile.d/functions.zsh
+++ etc/zprofile.d/functions.zsh
@@ -1,20 +1,16 @@
-alias hideme='history -d $((HISTCMD-1))'
-alias hideprev='history -d $((HISTCMD-2)) && history -d $((HISTCMD-1))'
-
-function hidegrep()
-{
+function airbrush () {
+  local histsize=$HISTSIZE
   if [ ! -z "$1" ]; then
-    while true; do
-      hist=$(history | grep -e "$1" | head -n 1)
-      if [ -z "$hist" ]; then break; fi
-      echo $hist
-      history -d $(echo $hist | awk '{ print $1 }' | sed 's/[^0-9]//g')
-    done
+    fc -AI
+    history | grep -e "$1"
+    HISTSIZE=0
+    sed -i -e '/'"$1"'/d' "$HISTFILE"
+    HISTSIZE=$histsize
+    fc -RI
   fi
 }
 
 # Launch screen with a name name and a fresh bash login.
-function scn ()
-{
+function scn () {
     screen -t $1 zsh -l
 }
