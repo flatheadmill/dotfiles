8d35aa5a206caf871430adb08b36b51b90f5ee5b
Alan Gutierrez <alan@prettyrobots.com>
2013-05-02 16:27:38 +0000

    Use `$HOME` instead of `/home/alan`.

--- etc/zprofile.zsh
+++ etc/zprofile.zsh
@@ -21,8 +21,8 @@ if [ -d /opt/bin ]; then
   PATH="/opt/bin:$PATH"
 fi
 
-if [ -d /Users/alan/.usr/share/npm/bin ]; then
-  PATH=/Users/alan/.usr/share/npm/bin:$PATH
+if [ -d $HOME/.usr/share/npm/bin ]; then
+  PATH=$HOME/.usr/share/npm/bin:$PATH
 fi
 
 if [ -e ~/.usr/bin ]; then
