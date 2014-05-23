9113c1cbf46a17a651b658dfc0328e7616752eca
Alan Gutierrez <alan@prettyrobots.com>
2014-05-23 08:57:15 -0700

    Fix `sed: RE error: illegal byte sequence`.

--- etc/zprofile.d/functions.zsh
+++ etc/zprofile.d/functions.zsh
@@ -3,7 +3,7 @@ function airbrush () {
   if [ ! -z "$1" ]; then
     history | grep -e "$1"
     HISTSIZE=0
-    sed -i -e '/'"$1"'/d' "$HISTFILE"
+    LC_ALL=C sed -i -e '/'"$1"'/d' "$HISTFILE"
     HISTSIZE=$histsize
     fc -R
   fi
