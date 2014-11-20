ae8f46d61db24d23b5ac541a3def3a4a6e678705
Alan Gutierrez <alan@prettyrobots.com>
2014-11-19 23:26:12 -0600

    Implement `cdf`.
    http://brettterpstra.com/2013/02/09/quick-tip-jumping-to-the-finder-location-in-terminal/

--- etc/zprofile.d/functions.zsh
+++ etc/zprofile.d/functions.zsh
@@ -8,3 +8,13 @@ function airbrush () {
     fc -R
   fi
 }
+
+# http://brettterpstra.com/2013/02/09/quick-tip-jumping-to-the-finder-location-in-terminal/
+function cdf () {
+    target=`osascript -e 'tell application "Finder" to if (count of Finder windows) > 0 then get POSIX path of (target of front Finder window as text)'`
+    if [ "$target" != "" ]; then
+        cd "$target"
+    else
+        echo 'No Finder window found' >&2
+    fi
+}
