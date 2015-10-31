69d645da76861389a69e339c774574126848ef79
Alan Gutierrez <alan@ljubljana.local>
2015-10-31 18:52:43 -0400

    Move configuration to separate program.

---
+++ configured.zsh
@@ -0,0 +1,5 @@
+curl https://raw.githubusercontent.com/bigeasy/dotfiles/master/install.zsh | zsh
+
+git config --global --set user.name 'Alan Gutierrez'
+git config --global --set user.email 'alan@prettyrobots.com'
+git config --global --set github.user 'bigeasy'
