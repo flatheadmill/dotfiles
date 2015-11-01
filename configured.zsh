9387042f370db859a9a46f9d473d870ef98bbfda
Alan Gutierrez <alan@ljubljana.local>
2015-10-31 21:52:45 -0400

    Fix `git` configuration.

--- configured.zsh
+++ configured.zsh
@@ -1,5 +1,5 @@
 curl https://raw.githubusercontent.com/bigeasy/dotfiles/master/install.zsh | zsh
 
-git config --global --set user.name 'Alan Gutierrez'
-git config --global --set user.email 'alan@prettyrobots.com'
-git config --global --set github.user 'bigeasy'
+git config --global --add user.name 'Alan Gutierrez'
+git config --global --add user.email 'alan@prettyrobots.com'
+git config --global --add github.user 'bigeasy'
