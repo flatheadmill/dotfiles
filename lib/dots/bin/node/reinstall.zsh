d20d25f9cd4c7b96317636799002a5f5e5b0ad32
Alan Gutierrez <alan@prettyrobots.com>
2017-10-26 02:57:37 -0500

    Reinstall.

---
+++ lib/dots/bin/node/reinstall.zsh
@@ -0,0 +1,8 @@
+#!/usr/bin/env zsh
+
+rm -rf node_modules
+npm install
+if git ls-files node_modules --error-unmatch 2>/dev/null; then
+    echo git checkout node_modules
+    git checkout node_modules
+fi
