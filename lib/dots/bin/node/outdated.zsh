90a8d3ab74b197be9d82e1128dab09c65b83a218
Alan Gutierrez <alan@prettyrobots.com>
2017-10-26 03:42:26 -0500

    Specify acceptable dist tags.

--- lib/dots/bin/node/outdated.zsh
+++ lib/dots/bin/node/outdated.zsh
@@ -12,16 +12,27 @@ usage
 
 set -e
 
+zparseopts -D -a o_dist d+: -dist+:
+
+if [[ ${#o_dist} -eq 0 ]]; then
+    o_dist=(-d latest)
+fi
+
+typeset -A dist
+dist=(${(Oa)o_dist})
+
+# https://stackoverflow.com/questions/22434290/jq-bash-make-json-array-from-variable
 outdated=$(npm outdated --json)
 for dependency in $(echo "$outdated" | jq -r '. | keys | .[]'); do
-    jq -s -r --arg dependency $dependency '
+    jq --arg dists "${(k)dist}" -s -r --arg dependency $dependency '
+        ($dists | split(" ")) as $dists |
         .[0] as $o2 |
-        .[1]["dist-tags"] | to_entries as $o1 |
+        [.[1]["dist-tags"] | to_entries[] | select(.key as $key | $dists | contains([$key]))] as $o1 |
         $o2 | to_entries[] | select(.key == $dependency) | .value.current as $current | {
             name: .key,
             current: .value.current,
             tags: [$o1[] | "\(.key) => \(.value)"] | join(", "),
             tag: [($o1[] | select(.value == $current) | .key), "OUTDATED"] | first
-        } | select(.tag == "OUTDATED") | "\(.name) \(.current) \(.tag) \(.tags)"
+        } | select(.tag == "OUTDATED") | "\(.name) \(.current) \(.tags)"
     ' <(echo "$outdated") <(npm info $dependency --json)
 done
