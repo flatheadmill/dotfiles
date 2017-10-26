ddb7d2ba32338358ef18882f6c8a134365e44fc9
Alan Gutierrez <alan@prettyrobots.com>
2017-10-26 02:20:00 -0500

    Coverage and test for 100% coverage.

---
+++ lib/dots/bin/node/coverage.zsh
@@ -0,0 +1,18 @@
+#!/usr/bin/env zsh
+
+pushd $(dirname ${(%):-%N}) > /dev/null
+dir=$PWD
+popd > /dev/null
+
+make -s -f "$dir/Makefile"
+
+jq -e '
+.total | [
+    .lines.pct, .statements.pct, .functions.pct, .branches.pct
+] | add | . == 400
+' < coverage/coverage-summary._json > /dev/null
+
+if [ $? -ne 0 ]; then
+    node_modules/.bin/istanbul report --format text    
+    exit 1
+fi
