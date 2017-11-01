c2db8aee33f48f6cadb243dfa00bc34366eb0295
Alan Gutierrez <alan@prettyrobots.com>
2017-11-01 13:19:00 -0400

    Restore `npm link` on `dots node reinstall`.

--- lib/dots/bin/node/reinstall.zsh
+++ lib/dots/bin/node/reinstall.zsh
@@ -1,8 +1,20 @@
 #!/usr/bin/env zsh
 
+links=()
+if [[ -d node_modules ]]; then
+    while read -r line; do
+        print will relink ${line#node_modules/}
+        links+=($line)
+    done < <(find node_modules -type l -path 'node_modules/*' -not -path 'node_modules/.bin/*')
+fi
+
 rm -rf node_modules
 npm install
 if git ls-files node_modules --error-unmatch 2>/dev/null; then
     echo git checkout node_modules
     git checkout node_modules
 fi
+
+for link in ${links[@]}; do
+    npm link ${link#node_modules/}
+done
