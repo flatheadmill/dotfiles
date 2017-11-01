bbee8215f36fbd665f381e2bc2a56d253a6c2687
Alan Gutierrez <alan@prettyrobots.com>
2017-11-01 13:08:52 -0400

    Do not use `npm outdated` in `dots node outdated`.
    `dots node outdated` no longer used `npm outdated` at all. Instead it
    maintains it's own cache of `dist-tags` information. Using this cache it
    will check each dependency instead of checking only those dependencies
    that where reported as out of date by `npm outdated`. `npm outdated`
    would not include "canary" or other `dist-tags` I use.

--- lib/dots/bin/node/outdated.zsh
+++ lib/dots/bin/node/outdated.zsh
@@ -12,27 +12,57 @@ usage
 
 set -e
 
-zparseopts -D -a o_dist d+: -dist+:
+o_expire=(-e 10)
+
+zparseopts -K -D -a o_dist d+: -dist+: -e:=o_expire
 
 if [[ ${#o_dist} -eq 0 ]]; then
     o_dist=(-d latest)
 fi
 
-typeset -A dist
-dist=(${(Oa)o_dist})
+typeset -A dists
+dists=(${(Oa)o_dist})
 
-# https://stackoverflow.com/questions/22434290/jq-bash-make-json-array-from-variable
-outdated=$(npm outdated --json)
-for dependency in $(echo "$outdated" | jq -r '. | keys | .[]'); do
-    jq --arg dists "${(k)dist}" -s -r --arg dependency $dependency '
-        ($dists | split(" ")) as $dists |
-        .[0] as $o2 |
-        [.[1]["dist-tags"] | to_entries[] | select(.key as $key | $dists | contains([$key]))] as $o1 |
-        $o2 | to_entries[] | select(.key == $dependency) | .value.current as $current | {
-            name: .key,
-            current: .value.current,
-            tags: [$o1[] | "\(.key) => \(.value)"] | join(", "),
-            tag: [($o1[] | select(.value == $current) | .key), "OUTDATED"] | first
-        } | select(.tag == "OUTDATED") | "\(.name) \(.current) \(.tags)"
-    ' <(echo "$outdated") <(npm info $dependency --json)
+function status_get_packages () {
+    local file=$1 collection=$2
+    # TODO Make $property an argument to `jq`.
+    jq --arg collection "$collection" -r '
+        [ . | to_entries[] | select(.key == $collection) | .value | to_entries[] | .key, .value ] | join(" ")
+    ' < "$file"
+}
+
+typeset -A packages
+packages=($(status_get_packages package.json dependencies) $(status_get_packages package.json devDependencies))
+
+CACHE=~/.usr/var/cache/dots/node/outdated/dist-tags
+
+find "$CACHE" -type f -mmin +"$o_expire[2]" -exec rm {} \;
+
+for dependency in ${(k)packages}; do
+    package=node_modules/$dependency/package.json
+    if [[ ! -e "$package" ]]; then
+        print -u2 "$package not installed"
+        exit 1
+    fi
+    version=$(jq -r '.version' <  "$package")
+    info="$CACHE"/"$dependency"/dist-tags.json 
+    mkdir -p "${info%/*}"
+    if [[ ! -e "$info" ]]; then
+        npm view "$dependency" dist-tags --json > "$info"
+    fi
+    typeset -A tags
+    tags=($(jq -r '[ . | to_entries[] | .key, .value ] | join(" ")' < "$info"))
+    found=0
+    for dist in ${(k)dists}; do
+        if [[ $version = $tags[$dist] ]]; then
+            found=1
+        fi
+    done
+    if [[ $found -eq 0 ]]; then
+        pairs=()
+        for tag release in ${(kv)tags}; do
+            pairs+=("$tag => $release")
+        done
+        print $dependency@$version $packages[$dependency] ${(j: :)pairs}
+    fi
 done
