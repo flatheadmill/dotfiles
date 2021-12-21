7f32cf360ead9cc9eae0f243c9d8eeae0ce998fc
Alan Gutierrez <alan@prettyrobots.com>
2021-12-21 01:15:48 -0600

    Add `gcloud` completions to `zsh`.

---
+++ etc/zprofile.zsh
@@ -0,0 +1,3 @@
+if [[ -e "$(brew --prefix)/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc" ]]; then
+    source "$(brew --prefix)/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc"
+fi
