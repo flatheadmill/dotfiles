aba67373bb444c3f4046ba4e689e19e8939207d4
Alan Gutierrez <alan@prettyrobots.com>
2022-02-22 06:25:30 +0000

    Reset `SSH_AUTH_SOCK`.

--- etc/zshrc.zsh
+++ etc/zshrc.zsh
@@ -8,3 +8,21 @@ unsetopt autopushd
 
 fpath=( ~/.dotfiles/share/zsh/functions "${fpath[@]}" )
 autoload -Uz fu
+
+# Reset the `SSH_AUTH_SOCK` before each command. Could also symlink it on
+# startup, but this works too.
+# https://www.babushk.in/posts/renew-environment-tmux.html
+if [ -n "$TMUX" ]; then
+  function refresh {
+    local ssh_auth_sock="$(tmux show-environment | grep "^SSH_AUTH_SOCK")"
+    if [[ -n "$ssh_auth_sock" ]]; then
+        export "$ssh_auth_sock"
+    fi
+  }
+else
+  function refresh {}
+fi
+
+function preexec {
+    refresh
+}
