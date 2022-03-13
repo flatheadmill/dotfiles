c364be67142a83eb503aad74c73c8fce047d048c
Alan Gutierrez <alan@prettyrobots.com>
2022-03-12 22:24:22 -0600

    Source `~/.nvm` stuff it exists.

--- etc/zprofile.zsh
+++ etc/zprofile.zsh
@@ -1,3 +1,9 @@
 if [[ $OSTYPE == darwin* && -e "$(brew --prefix)/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc" ]]; then
     source "$(brew --prefix)/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc"
 fi
+
+if [[ -e "$HOME/.nvm" ]]; then
+    export NVM_DIR="$HOME/.nvm"
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+    [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+fi
