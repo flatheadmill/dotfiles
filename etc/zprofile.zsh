5aed3eeb0eff014cade7831f398135eaad71184b
Alan Gutierrez <alan@prettyrobots.com>
2022-03-16 11:33:23 +0000

    Source `gvm` if it is installed.

--- etc/zprofile.zsh
+++ etc/zprofile.zsh
@@ -7,3 +7,7 @@ if [[ -e "$HOME/.nvm" ]]; then
     [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
     [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 fi
+
+if [[ -e "$HOME/.gvim/scripts/gvm" ]]; then
+    source /home/homeport/.gvm/scripts/gvm
+fi
