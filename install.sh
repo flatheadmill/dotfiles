03e51e338d932ba0e6ba11f8ee4e95cebce4453e
Alan Gutierrez <alan@prettyrobots.com>
2022-04-18 01:33:00 -0500

    Thoughts on a faster installer.

---
+++ install.sh
@@ -0,0 +1,11 @@
+# TODO Notes on a faster installer.
+#
+# This part would run
+#   sudo sh -c "$(wget -qO - https://flatheadmill.github.io/install.sh)"
+# and that would redirect to the right place.
+#
+# It would detect the platform and distribution and then install all the basic
+# dependencies and any missing locales.
+#
+# It would then call the `zsh` installer. It's been years and I haven't had to
+# install this on a machine where I didn't have root.
