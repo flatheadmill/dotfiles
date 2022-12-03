a420d0bb4affee90d59131c516d7fec11f5768ba
Alan Gutierrez <alan@prettyrobots.com>
2022-12-03 14:21:29 -0600

    Set Zsh options, keep old options for posterity.

--- etc/zshrc.zsh
+++ etc/zshrc.zsh
@@ -1,28 +1,79 @@
-## Completions.
+# +-------------+
+# | Environment |
+# +-------------+
 
-# TODO Why is this one special?
+export EDITOR=vim               # Set the default editor.
+export HISTSIZE=1000           # As much history in memory as possible.
+export SAVEHIST=10000           # As much history saved to disk as is possible.
+export HISTFILESIZE=10000
+
+# +-----------------+
+# | History Options |
+# +-----------------+
+
+# Want all my `tmux` sessions to record history for posterity, but we don't want
+# them loading up each others commands because that gets confusing.
+
+setopt HIST_EXPIRE_DUPS_FIRST   # Delete duplicates before deleting the tail of history.
+setopt HIST_FIND_NO_DUPS        # Skip a duplicate when performing a reverse search.
+setopt HIST_IGNORE_SPACE        # Do not record command lines where leading character is a space.
+setopt HIST_VERIFY              # Don't just execute the command, load it into the line buffer.
+setopt INC_APPEND_HISTORY       # Incrementally append history, allows us to feed history from many `tmux` panes.
+
+setopt ALWAYS_TO_END            # Move cursor to end of completed word. TODO Disable and observe.
+setopt BASH_AUTO_LIST           # Auto list on a double tab.
+setopt COMBINING_CHARS          # TODO Possibly requires a test before setting.
+setopt COMPLETE_IN_WORD         # This will expand `fbar` to `foobar` when the cursor is at the end of the word.
+setopt NO_FLOW_CONTROL          # Something to do with silencing the terminal. TODO Enable and observe and research.
+setopt INTERACTIVE              # Insist that this is an interactive shell.
+setopt INTERACTIVE_COMMENTS     # Allow comments even in interactive shells.
+setopt LOGIN                    # Insist that this a login shell.
+setopt LONG_LIST_JOBS           # Print job notifications in the long format by default.
+setopt MONITOR                  # Allow job control. Set by default in interactive shells.
+setopt PROMPT_SUBST             # Perform parameter expansion, command substitution, and arithmetic expansion in prompts.
+                                # TODO ^ Do I need this when I'm using `minimal.zsh`?
+setopt PUSHD_IGNORE_DUPS        # Don’t push multiple copies of the same directory onto the directory stack.
+setopt PUSHD_MINUS              # Transpose the meanings of `+` and `-` when referencing the stack.
+
+# +-------------+
+# | Completions |
+# +-------------+
+
+_comp_options+=(globdots)       # With hideen files.
+# TODO Why is this one special? Can't be right.
 aws_zsh_completer=$(which aws_zsh_completer.sh)
 if [[ $? -eq 0 ]]; then
     source $aws_zsh_completer
 fi
 
-if type brew &>/dev/null
-then
+# TODO Where do I put my Unix completions?
+if whence brew > /dev/null; then
     fpath=( "$(brew --prefix)/share/zsh/site-functions" "${fpath[@]}" )
 fi
 
+# Dubious.
 fpath=( ~/.dotfiles/share/zsh/functions "${fpath[@]}" )
 if [[ -d ~/.usr/share/zsh/functions ]]; then
     fpath=( ~/.usr/share/zsh/functions "${fpath[@]}" )
 fi
 
+autoload -U compinit; compinit
+
+# Outgoing?
 autoload -Uz fu
 
 source $HOME/.dotfiles/vendor/minimal.zsh
 
+# +---------------------+
+# | Prompt Interceptors |
+# +---------------------+
+
 # Reset the `SSH_AUTH_SOCK` before each command. Could also symlink it on
 # startup, but this works too.
+#
 # https://www.babushk.in/posts/renew-environment-tmux.html
+
+#
 if [ -n "$TMUX" ]; then
   function refresh {
     local ssh_auth_sock="$(tmux show-environment | grep "^SSH_AUTH_SOCK")"
@@ -46,10 +97,6 @@ my_precmd() {
   fi
 }
 
-export HISTSIZE=1000000000
-export HISTFILESIZE=1000000000
-#
-#setopt INC_APPEND_HISTORY
 
 bindkey '^R' history-incremental-pattern-search-backward 
 
@@ -64,5 +111,3 @@ mnml_ssh() {
         fi
     fi
 }
-
-export EDITOR=vim
