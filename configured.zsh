a0a0c0ac84b65d5a6b8893b95136b9f83c482b81
Alan Gutierrez <alan@prettyrobots.com>
2022-12-13 13:21:07 -0600

    Change GitHub user name.

--- configured.zsh
+++ configured.zsh
@@ -8,4 +8,4 @@ fi
 
 git config --file ~/.dotfiles/rc/gitconfig --add user.name 'Alan Gutierrez'
 git config --file ~/.dotfiles/rc/gitconfig --add user.email 'alan@prettyrobots.com'
-git config --file ~/.dotfiles/rc/gitconfig --add github.user 'bigeasy'
+git config --file ~/.dotfiles/rc/gitconfig --add github.user 'flatheadmill'
