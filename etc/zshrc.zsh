3376a211e507014db9836b85fceae18aa0e9570f
Alan Gutierrez <alan@prettyrobots.com>
2023-05-04 10:08:55 -0500

    Restore `airbrush` function.

--- etc/zshrc.zsh
+++ etc/zshrc.zsh
@@ -121,3 +121,14 @@ mnml_ssh() {
         fi
     fi
 }
+
+function airbrush {
+  local histsize=$HISTSIZE
+  if [ ! -z "$1" ]; then
+    history | grep -e "$1"
+    HISTSIZE=0
+    LC_ALL=C sed -i -e '/'"$1"'/d' "$HISTFILE"
+    HISTSIZE=$histsize
+    fc -R
+  fi
+}
