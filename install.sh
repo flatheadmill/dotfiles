4743100db470a3a59fefa37a5a819c973fd7f0f7
Alan Gutierrez <alan@prettyrobots.com>
2025-04-19 18:35:51 +0000

    Order rc sweep, clone tidy.

--- install.sh
+++ install.sh
@@ -9,6 +9,8 @@ abend() {
     exit 1
 }
 
+# Emplace an rc file in the home directory and sweep the existing file to a
+# recycle bin in case we need to fish something out of it.
 create_rc() {
     local home_file=$1 skel_file=$2
     local home_path="$HOME/$home_file" skel_path="$HOME/.dotfiles/skel/$skel_file"
@@ -29,6 +31,17 @@ create_rc() {
     fi
 }
 
+# Clone only if the directory does not exist.
+git_clone() {
+    local directory=$1 url=$2
+    shift 2
+    git ls-remote "$url" unlikely_reference || abend 'unable to reach `%s`' "$url"
+    if [ ! -d "$directory" ]; then
+        git clone "$@" --quiet "$url" "$directory" || abend 'unabled to clone `%s`.' "$url"
+    fi
+}
+
+# Let use know we replaced a default rc file.
 announce_copy() {
 cat <<EOF
 Existing configuration files where replaced. The replaced files have been
@@ -49,9 +62,11 @@ This is where your machine specific settings should be kept.
 EOF
 }
 
+# Here we go.
 main() {
     # `git` is required to install.
     which git > /dev/null || abend 'git is not installed' 
+    # Assert a reasonable `git` version.
     local git_version
     git_version=$(git version) || abend 'cannot read git version'
     git_version=${git_version##* }
@@ -62,6 +77,33 @@ main() {
         mkdir -p ~/.ssh || abend 'cannot create `~/.ssh`'
         touch ~/.ssh/known_hosts || abend 'cannot create `~/.ssh/known_hosts`'
     )
+    # Seed our `~/.ssh/known_hosts` with the GitHub public key.
+    if [ -z "$(ssh-keygen -F github.com)" ]; then
+        local key hosts
+        # Get the public key from the `github.com` server.
+        key=$(ssh-keyscan -t ed25519 github.com 2> /dev/null | cut -f3 -d' ') ||
+            abend 'unable to fetch `github.com` SSH keys'
+        # Create a comma delimited list of host ip addresses and the domain.
+        hosts=$(printf '%sgithub.com' "$(dig -t a +short github.com  | tr '\n' ',')") ||
+            abend 'unable to resolve `github.com` DNS'
+        # Get that fingerprint.
+        fp=$(printf '%s ssh-ed25519 %s' "$hosts" "$key" | ssh-keygen -qlf - | cut -f2 -d' ')
+        # https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/githubs-ssh-key-fingerprints
+        [ "$fp" = "SHA256:+DiY3wvvV6TuJJhbpZisF/zLDA0zPMSvHdkr4UvCOqU" ] ||
+            abend 'bad `github.com` SSH fingerprint %s' "$fp"
+        printf '%s ssh-ed25519 %s\n' $hosts $key >> ~/.ssh/known_hosts
+    fi
+    # Assert that we can call `git` for our private `dotfiles` repository.
+    # https://superuser.com/questions/227509/git-ping-check-if-remote-repository-exists
+    git ls-remote git@github.com:flatheadmill/dotfiles.git unlikely_reference ||
+        abend 'unable to reach `flatheadmill/dotfiles.git`, did you forget to forward SSH?'
+    # Clone dotfiles.
+    git_clone "$HOME/.dotfiles" "git@github.com:flatheadmill/dotfiles.git" --recursive
+    # Could as easily be in my standard config, but I keep it here to
+    # remind myself that this is how you tweak local installations.
+    git config --file ~/.local/etc/gitconfig --add user.name 'Alan Gutierrez'
+    git config --file ~/.local/etc/gitconfig --add user.email 'alan@prettyrobots.com'
+    git config --file ~/.local/etc/gitconfig --add github.user 'flatheadmill'
     # Timestamp for our rc file sweep.
     local stamp=$(date +'%F-%T' | sed 's/:/-/g')
     # Emplace our Zsh configuration.
@@ -82,29 +124,6 @@ main() {
     # Emplace our `ssh` configuration.
     create_rc .ssh/config ssh/config
     touch ~/.ssh/homeport.config
-    if [ -z "$(ssh-keygen -F github.com)" ]; then
-        local key hosts
-        # Get the public key from the `github.com` server.
-        key=$(ssh-keyscan -t ed25519 github.com 2> /dev/null | cut -f3 -d' ') ||
-            abend 'unable to fetch `github.com` SSH keys'
-        # Create a comma delimited list of host ip addresses and the domain.
-        hosts=$(printf '%sgithub.com' "$(dig -t a +short github.com  | tr '\n' ',')") ||
-            abend 'unable to resolve `github.com` DNS'
-        # Get that fingerprint.
-        fp=$(printf '%s ssh-ed25519 %s' "$hosts" "$key" | ssh-keygen -qlf - | cut -f2 -d' ')
-        # https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/githubs-ssh-key-fingerprints
-        [ "$fp" = "SHA256:+DiY3wvvV6TuJJhbpZisF/zLDA0zPMSvHdkr4UvCOqU" ] ||
-            abend 'bad `github.com` SSH fingerprint %s' "$fp"
-        printf '%s ssh-ed25519 %s\n' $hosts $key >> ~/.ssh/known_hosts
-    fi
-    # Assert that we can call `git`.
-    # https://superuser.com/questions/227509/git-ping-check-if-remote-repository-exists
-    git ls-remote git@github.com:flatheadmill/dotfiles.git unlikely_reference ||
-        abend 'unable to reach `flatheadmill/dotfiles.git`, did you forget to forward SSH?'
-    # Clone dotfiles.
-    if [ ! -e "$HOME/.dotfiles" ]; then
-        git clone --recursive git@github.com:flatheadmill/dotfiles.git "$HOME/.dotfiles"
-    fi
     # Create our `~/.local` filesystem.
     mkdir -p ~/.local/etc/zshrc.d
     local dir
@@ -112,29 +131,14 @@ main() {
         mkdir -p ~/.local/$dir
     done
     touch ~/.local/var/tmux.run.log
+    mkdir -p ~/.local/etc/zshrc.d
     curl -sSL https://raw.githubusercontent.com/subnixr/minimal/master/minimal.zsh > ~/.local/etc/zshrc.d/minimal.zsh
-    if [ ! -d ~/.tmux/plugins/tpm ]; then
-        git ls-remote https://github.com/tmux-plugins/tpm unlikely_reference ||
-            abend 'unable to reach `tmux-plugins/tpm.git`'
-        git clone -q https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-    fi
+    git_clone $HOME/.tmux/plugins/tpm https://github.com/tmux-plugins/tpm
     # Bash configuration.
     mkdir -p ~/.config/bash/plugins
-    git_clone ~/.config/bash/plugins/aphrodite https://github.com/win0err/aphrodite-terminal-theme
-    # Could as easily be in my standard config, but I keep it here to
-    # remind myself that this is how you tweak local installations.
-    git config --file ~/.local/etc/gitconfig --add user.name 'Alan Gutierrez'
-    git config --file ~/.local/etc/gitconfig --add user.email 'alan@prettyrobots.com'
-    git config --file ~/.local/etc/gitconfig --add github.user 'flatheadmill'
+    git_clone $HOME/.config/bash/plugins/aphrodite https://github.com/win0err/aphrodite-terminal-theme
     if [ -d "$HOME/.local/var/dotfiles/replaced/$stamp" ]; then
         announce_copy
-    fi
-}
-
-git_clone() {
-    local directory=$1 url=$2
-    if [ ! -d "$directory" ]; then
-        git clone --quiet "$url" "$directory" || abend 'unabled to clone `%s`.' "$url"
     fi
 }
 
