7b3bf3fdea2710d1d47cf4471d982fa4a8228a14
Alan Gutierrez <alan@prettyrobots.com>
2025-10-28 18:53:44 -0500

    Auto-set cargo compiler for Rust files.
    Added compiler cargo to Rust FileType autocmd so :make! automatically works with build/test/check/clippy subcommands with proper error parsing.

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -15,10 +15,13 @@ o.wildmode = 'longest:full,full'
 -- Disable persistent undo (file-based undo history)
 o.undofile = false
 
--- Remove ! from spellcapcheck for Rust to handle //! doc comments
+-- Rust-specific settings
 vim.api.nvim_create_autocmd("FileType", {
   pattern = "rust",
   callback = function()
+    -- Remove ! from spellcapcheck for //! doc comments
     vim.opt_local.spellcapcheck = [[.[?]\_[\])'"	 ]\+]]
+    -- Set cargo as compiler for :make! build/test/check/clippy
+    vim.cmd("compiler cargo")
   end,
 })
