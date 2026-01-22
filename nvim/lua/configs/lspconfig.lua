321745860d2978bd7efecbc81632db7d1ef70db0
Alan Gutierrez <alan@prettyrobots.com>
2026-01-21 20:01:24 -0600

    Remove tabs.

--- nvim/lua/configs/lspconfig.lua
+++ nvim/lua/configs/lspconfig.lua
@@ -7,11 +7,11 @@ vim.lsp.enable(servers)
 -- Rust is handled by rustaceanvim plugin, not manual lspconfig 
 
 vim.lsp.config.clangd = {
-	cmd = { "clangd" },
-	filetypes = { "c", "cpp", "objc", "objcpp" },
-	root_markers = { "compile_commands.json", ".clangd", ".git" },
-	on_attach = require("nvchad.configs.lspconfig").on_attach,
-	capabilities = require("nvchad.configs.lspconfig").capabilities,
+    cmd = { "clangd" },
+    filetypes = { "c", "cpp", "objc", "objcpp" },
+    root_markers = { "compile_commands.json", ".clangd", ".git" },
+    on_attach = require("nvchad.configs.lspconfig").on_attach,
+    capabilities = require("nvchad.configs.lspconfig").capabilities,
 }
 
 vim.lsp.enable("clangd")
